ae69650ef83091a543f0d493d7074ca9d76f5537
Sanath <sanaabdulmanaf88@gmail.com>
2022-12-05 18:59:24 +0530

    Export modules

--- Module.js
+++ Module.js
@@ -1,34 +1,29 @@
-module.exports="My Exported Modules"
+// module.exports="My Exported Modules"
 
 
 
-// const fs=require("fs");
-// const path=require("path");
+const fs=require("fs");
+const path=require("path");
 
 
-// function getFilePath(){
-//     return path.join(process.cwd(),"file.txt");
-// }
+function getFilePath(){
+    return path.join(process.cwd(),"file.txt");
+}
 
-// function writeToFile(filePath,string)
-// {
-//     fs.writeFileSync(filePath,string);
-// }
-// function readFromFile(filePath){
-//     return fs.readFileSync(filePath).toString();//fs.writeFileSync(filePath,string);
-// }
-// function readFromFile(filePath){
-//     return fs.readFileSync(filePath).toString();
-
-// }
-
-// exports.getFilePath=getFilePath;
-// exports.writeToFile=writeToFile;
-// exports.readFromFile=readFromFile;ilePath()));
+function writeToFile(filePath,string)
+{
+    fs.writeFileSync(filePath,string);
+}
+function readFromFile(filePath){
+    return fs.readFileSync(filePath).toString();
+}
 
 
-// }
 
-// exports.getFilePath=getFilePath;
-// exports.writeToFile=writeToFile;
-// exports.readFromFile=readFromFile;
+exports.getFilePath=getFilePath;
+exports.writeToFile=writeToFile;
+exports.readFromFile=readFromFile;
+
+
+
+
--- app.js
+++ app.js
@@ -6,19 +6,29 @@ function getName(name){
 
 getName("Sanath");
 
-//modules - file systems
-
-const fs = require("fs")
-const path=require("path");
 
 const myModule=require("./Module"); 
 
-const filePath= path.join(process.cwd(),"file.txt");
-fs.writeFileSync(filePath,"My first file");    //to write a content into the file 
 
-const fileContent=fs.readFileSync(filePath).toString();  // to read a content in a file
-console.log(fileContent);
+myModule.writeToFile(myModule.getFilePath(),"Updated from App.js");
+console.log(myModule.readFromFile(myModule.getFilePath()));
 
-console.log(myModule);
+
+
+
+
+//modules - file systems
+
+// const fs = require("fs")
+// const path=require("path");
+
+
+// const filePath= path.join(process.cwd(),"file.txt");
+// fs.writeFileSync(filePath,"My first file");    //to write a content into the file 
+
+// const fileContent=fs.readFileSync(filePath).toString();  // to read a content in a file
+// console.log(fileContent);
+
+//console.log(myModule);
 
 
