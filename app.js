95bf124852ae14bbcc767b86417f7311225b1913
Sanath <sanaabdulmanaf88@gmail.com>
2022-12-06 12:52:05 +0530

    creating web server/http server with node js

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
-console.log("Hello")
+const express=require("express");
+ 
 
-function getName(name){
-    console.log("My name is ",name);
-}
+const app=express()
 
-getName("Sanath");
-
-
-const myModule=require("./Module"); 
+app.use('/',(req,res,next)=>{
+    res.send("Hi I am Sanath")
+})
 
 
-myModule.writeToFile(myModule.getFilePath(),"Updated from App.js");
-console.log(myModule.readFromFile(myModule.getFilePath()));
-
-
-
-
-
-//modules - file systems
-
-// const fs = require("fs")
-// const path=require("path");
-
-
-// const filePath= path.join(process.cwd(),"file.txt");
-// fs.writeFileSync(filePath,"My first file");    //to write a content into the file 
-
-// const fileContent=fs.readFileSync(filePath).toString();  // to read a content in a file
-// console.log(fileContent);
-
-//console.log(myModule);
-
-
+app.listen(5000,()=>{
+    console.log("Connected to the port 5000");
+})
